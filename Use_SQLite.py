4b3be6165a960d9813f047663de71b0587670d7f
zzhang401 <pleiades401@gmail.com>
2018-09-14 09:34:20 -0600

    Solution to Use SQLite

---
+++ Use_SQLite.py
@@ -0,0 +1,32 @@
+#!/user/bin/env python 3
+# -*- coding: utf-8 -*-
+___author___='Zhenzihao zhang'
+
+import os, sqlite3
+
+db_file = os.path.join(os.path.dirname(__file__), 'test.db')
+if os.path.isfile(db_file):
+    os.remove(db_file)
+
+# 初始数据:
+conn = sqlite3.connect(db_file)
+cursor = conn.cursor()
+cursor.execute('create table user(id varchar(20) primary key, name varchar(20), score int)')
+cursor.execute(r"insert into user values ('A-001', 'Adam', 95)")
+cursor.execute(r"insert into user values ('A-002', 'Bart', 62)")
+cursor.execute(r"insert into user values ('A-003', 'Lisa', 78)")
+cursor.close()
+conn.commit()
+conn.close()
+
+def get_score_in(low, high):
+    ' 返回指定分数区间的名字，按分数从低到高排序 '
+    conn=sqlite3.connect('test.db')
+    cursor=conn.cursor()
+    cursor.execute('select * from user where score>? and score<=?',(low,high))
+    values=cursor.fetchall()
+    return values
+    pass
+
+given_name=get_score_in(61,78)
+print(given_name)
